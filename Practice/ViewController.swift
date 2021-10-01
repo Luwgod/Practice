2ca2d17ce663b996f85dbe9c7e27bb1e750c15dc
Alex Styazhkin <iq98@list.ru>
2021-10-01 14:07:38 +0300

    Initial Commit

---
+++ Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice
+//
+//  Created by Sasha Styazhkin on 01.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
